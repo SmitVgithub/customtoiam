4ded97793161854a1caab10a712669135149dda8
Smit Vaghasiya <146537968+SmitVgithub@users.noreply.github.com>
2024-03-28 19:50:53 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ def index():
 @app.route('/api/request', methods=['GET'])
 def send_request():
     # You can add any necessary logic here
-    return '200 OK'
+    return 'OK Done'
 
 if __name__ == '__main__':
     app.run(debug=True)
